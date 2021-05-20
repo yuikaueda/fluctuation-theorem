8f04c90a664c65b26a9b2f4182a53afeb52ef9d5
yuikaueda <yuika428@gmail.com>
2021-05-20 13:48:32 +0900

    n0520

--- Fac_Fmy/var_1_n-1/experiment/lngraph.py
+++ Fac_Fmy/var_1_n-1/experiment/lngraph.py
@@ -5,18 +5,18 @@ import math
 
 df = np.loadtxt('sarcomerelength.dat')
 df1 = df.round(1) - 1.12
-num = 18
+num = 34
 
-a, b, _ = plt.hist(df1, bins=num, range=(-0.9,2.6), density=True)
+a, b, _ = plt.hist(df1, bins=num, range=(-0.75,2.65), density=True)
 plt.close()
 #print(a)
 aa = []
 bb = []
 
 step0=0
-for i in range(0, 3):
-    if not a[i] == 0 and not a[i+8-step0] == 0:
-        a_hi = math.log(a[i]/a[i+8-step0])
+for i in range(0, 7):
+    if not a[i] == 0 and not a[i+14-step0] == 0:
+        a_hi = math.log(a[i]/a[i+14-step0])
         aa.append(a_hi)
         bb.append(b[i])
     step0+=2
@@ -25,10 +25,10 @@ for i in range(0, 3):
 
 a_hi = 0
 aa.append(a_hi)
-bb.append(b[4])
+bb.append(0)
 
 step=2
-for i in range(5, 8):
+for i in range(8, 15):
     if not a[i] == 0 and not a[i-step] == 0: 
         a_hi = math.log(a[i]/a[i-step])
         aa.append(a_hi)
@@ -52,5 +52,5 @@ fig, ax = plt.subplots(1, 1)
 ax.plot(bb, aa, 'o', c='black')
 plt.xlabel(r"$sarcomere\ length[\mu m]$", fontsize = 18)
 plt.ylabel(r"$ln[P( \Delta x) / P(- \Delta x)]$", fontsize = 18)
-fig.savefig("lnp_range_ln_st02_round1_bins18")
+fig.savefig("lnp_range_ln_st02_round1_bins34")
 plt.show()
